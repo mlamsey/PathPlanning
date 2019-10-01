12a275c8db14eccda0b5a3a25830798aafe7f150
mlamsey <pty883@vols.utk.edu>
2019-10-01 12:31:03 -0400

    updates segment constructor arg errors

--- PlanObjects/Segment.m
+++ PlanObjects/Segment.m
@@ -6,9 +6,7 @@ classdef Segment
 
 		function obj = Segment(slices)
 			if(~isa(slices,'cell'))
-				disp('c')
 				fprintf('Segment::Segment: Input not a cell array!\n');
-				disp('d')
 				return;
 			end%if
 			if(~Utils.AreAll(slices,'Slice'))
