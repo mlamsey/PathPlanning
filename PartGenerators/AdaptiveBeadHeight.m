6dd06ab02f7227319df1f4cb9a71ddce7b70d67f
mlamsey <pty883@vols.utk.edu>
2020-07-21 10:38:03 -0400

    updates gitignore, changes sandbox config

--- PartGenerators/AdaptiveBeadHeight.m
+++ PartGenerators/AdaptiveBeadHeight.m
@@ -1,5 +1,5 @@
 function output = AdaptiveBeadHeight
-	close all;
+	% close all;
 	% normal_vector = [0,0,1];
 	min_layer_height = 2.25;
 	max_layer_height = 2.75;
@@ -8,9 +8,9 @@ function output = AdaptiveBeadHeight
 	part_length = 5 * 25.4; % in -> mm
 	lump_height = 1 * 25.4; % in -> mm
 
-	layers = WallGenerator.GenerateGaussLayerPart(lump_height,part_length,min_layer_height,max_layer_height);
+	% layers = WallGenerator.GenerateGaussLayerPart(lump_height,part_length,min_layer_height,max_layer_height);
 	% AdaptiveBeadPlotTools.PlotLayers(layers);
-	AdaptiveBeadPlotTools.PlotGaussHeat(layers,min_layer_height);
+	% AdaptiveBeadPlotTools.PlotGaussHeat(layers,min_layer_height);
 	% GeometryProcessor.UpdateWallLayerNormals(layers{1});
 
 	% Pringle part 1
@@ -23,6 +23,46 @@ function output = AdaptiveBeadHeight
 	% layers = CylinderGenerator.GeneratePringleLayerPart(part_perturbation,part_radius,n_perturbations,min_layer_height,max_layer_height);
 	% AdaptiveBeadPlotTools.PlotLayers(layers);
 	% GeometryProcessor.UpdateCylinderLayerNormals(layers{1});
-
-	output = layers;
+	TestHeatEqn;
+	% output = layers;
 end%func AdaptiveBeadHeight
+
+function TestHeatEqn
+	% real parameters
+	part_length = 6 * 25.4; % in -> mm
+	part_height = 150; % mm
+	interval_size = 0.5; % mm
+	decay_rate = 200; % units?
+	n_points = 50;
+	x = linspace(0,part_length,n_points);
+
+	% heat equation
+	L = part_length;
+	t = part_height;
+	k = decay_rate;
+	n = n_points;
+	t_steps = ceil(part_height / interval_size);
+	dx = L / n;
+	dt = 1 / t_steps;
+	% a = k * (dt / dx ^ 2) % alpha
+	a = 0.5;
+	
+	t0 = sin(0.05 .* x);
+	t1 = zeros(1,n_points);
+	t0(1) = 0;
+	t0(end) = 1;
+
+	for i = 1:t_steps
+		for j = 2:n-1
+			t1(j) = t0(j) + a*(t0(j+1) - 2*t0(j) + t0(j-1));
+		end%for j
+		plot(x,t1);
+		ylim([-1,1]);
+		title(sprintf('%i',i));
+		pause(0.01);
+		t0 = t1;
+	end%for i
+
+	
+
+end%func TestHeatEqn
